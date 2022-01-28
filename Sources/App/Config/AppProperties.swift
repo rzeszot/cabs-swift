80406242a4750e94a7cc8f3f49fe0e5f6898c251
Damian Rzeszot <damian.rzeszot@gmail.com>
2022-01-28 03:11:45 +0100

    Common/AppProperties

---
+++ Sources/App/Config/AppProperties.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+class AppProperties {
+    var noOfTransitsForClaimAutomaticRefund: Int = 5
+    var automaticRefundForVipThreshold: Int = 2
+    var minNoOfCarsForEcoClass: Int = 3
+
+    var milesExpirationInDays: Int = 365
+    var defaultMilesBonus: Int = 10
+}
